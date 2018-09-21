7e60234b0b6dd775864bb9e0c46de8eb53bf2fb7
Törmikoski Lassi TTK17SP <lassitormikoski@kamk.fi>
2018-09-21 09:55:17 +0300

    Project Init

---
+++ MrOrb/Intermediate/Build/Win64/UE4Editor/Inc/MrOrb/MrOrbGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MRORB_MrOrbGameModeBase_generated_h
+#error "MrOrbGameModeBase.generated.h already included, missing '#pragma once' in MrOrbGameModeBase.h"
+#endif
+#define MRORB_MrOrbGameModeBase_generated_h
+
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_RPC_WRAPPERS
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMrOrbGameModeBase(); \
+	friend MRORB_API class UClass* Z_Construct_UClass_AMrOrbGameModeBase(); \
+public: \
+	DECLARE_CLASS(AMrOrbGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MrOrb"), NO_API) \
+	DECLARE_SERIALIZER(AMrOrbGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMrOrbGameModeBase(); \
+	friend MRORB_API class UClass* Z_Construct_UClass_AMrOrbGameModeBase(); \
+public: \
+	DECLARE_CLASS(AMrOrbGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MrOrb"), NO_API) \
+	DECLARE_SERIALIZER(AMrOrbGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMrOrbGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMrOrbGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMrOrbGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMrOrbGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMrOrbGameModeBase(AMrOrbGameModeBase&&); \
+	NO_API AMrOrbGameModeBase(const AMrOrbGameModeBase&); \
+public:
+
+
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMrOrbGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMrOrbGameModeBase(AMrOrbGameModeBase&&); \
+	NO_API AMrOrbGameModeBase(const AMrOrbGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMrOrbGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMrOrbGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMrOrbGameModeBase)
+
+
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_12_PROLOG
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_RPC_WRAPPERS \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_INCLASS \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	MrOrb_Source_MrOrb_MrOrbGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MrOrb_Source_MrOrb_MrOrbGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
