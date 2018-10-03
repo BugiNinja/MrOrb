13bb68f561f0de48e1ea0bed4f1d5aed4a901b04
JanneSil <31925875+JanneSil@users.noreply.github.com>
2018-10-03 14:51:59 +0300

    Score System

---
+++ MrOrb/Source/MrOrb/ScoreSystemComponent.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ScoreSystemComponent.h"
+
+
+// Sets default values for this component's properties
+UScoreSystemComponent::UScoreSystemComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	DefaultScore = 0;
+	ScoreToAdd = 10;
+}
+
+
+// Called when the game starts
+void UScoreSystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentScore = DefaultScore;
+	SetScore();
+}
+
+
+// Called every frame
+void UScoreSystemComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UScoreSystemComponent::AddScore()
+{
+	CurrentScore += ScoreToAdd;
+	SetScore();
+	return;
+}
+
+void UScoreSystemComponent::ResetScore()
+{
+	CurrentScore = 0;
+	SetScore();
+	return;
+}
+
+int UScoreSystemComponent::GetCurrentScore()
+{
+	return CurrentScore;
+}
+
+void UScoreSystemComponent::SetScore()
+{
+	DefaultScoreInString = FString::FromInt(CurrentScore);
+	return;
+}
+
+
