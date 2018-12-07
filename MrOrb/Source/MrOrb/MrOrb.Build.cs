120bffd3aa1618fd94be6641496988cb7664a9a1
JanneSil <31925875+JanneSil@users.noreply.github.com>
2018-12-07 15:28:55 +0200

    Everything

--- MrOrb/Source/MrOrb/MrOrb.Build.cs
+++ MrOrb/Source/MrOrb/MrOrb.Build.cs
@@ -8,14 +8,21 @@ public class MrOrb : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "OnlineSubsystem" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        //PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PrivateDependencyModuleNames.Add("OnlineSubsystemGooglePlay");
+            PrivateDependencyModuleNames.Add("OnlineSubsystem");
+            PrivateDependencyModuleNames.Add("AndroidAdvertising");
+        }
+    }
 }
